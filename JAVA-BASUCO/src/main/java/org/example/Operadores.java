82dcf087b3cde41882752d2be9a78639037ad96e
eduardodio23 <eduardo.f.jesus@ba.estudante.senai.br>
2026-08-11 21:23:28 -0300

    Add files via upload

---
+++ JAVA-BASUCO/src/main/java/org/example/Operadores.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Operadores {
+    static void main(String[] args) {
+        //Declaração de variáveis
+        int a = 10;
+        int b = 2;
+
+        // Operadores aritméticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+    }
+}
