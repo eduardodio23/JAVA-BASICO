a14c1a28ee95679bb6fb30e16a867428175fed7e
eduardodio23 <eduardo.f.jesus@ba.estudante.senai.br>
2026-08-17 21:25:44 -0300

    Add files via upload

---
+++ JAVA-BASICO-main/JAVA-BASUCO/src/main/java/org/example/Lacosderepeticao.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Lacosderepeticao {
+    static void main(String[] args) {
+        // enquanto i for menor que 6 i+1
+        for(int i = 0; i < 6; i++){
+            System.out.println("Valor de i:" + i);
+        }
+    }
+}
