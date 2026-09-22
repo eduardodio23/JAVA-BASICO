ad6d5a608c8885e8c38a8ee309e6ecd8fa158274
eduardodio23 <eduardo.f.jesus@ba.estudante.senai.br>
2026-09-21 21:16:37 -0300

    Add files via upload

---
+++ senac/src/main/java/com/example/senac/entity/ClienteEntity.java
@@ -0,0 +1,30 @@
+package com.example.senac.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tab_cliente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(nullable = false)
+    private  String nome;
+
+    @Column(nullable = false)
+    private  String email;
+
+    @Column(nullable = false)
+    private  String telefone;
+
+    @Column(nullable = false)
+    private  String endereco;
+
+
+}
